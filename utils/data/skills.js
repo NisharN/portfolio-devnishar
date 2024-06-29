eb869a406dc629038b82b3b9c3d1c8bccc323b30
de-salman <salmanfarisi.bca@gmail.com>
2024-06-30 00:20:31 +0530

    forth commit

--- utils/data/skills.js
+++ utils/data/skills.js
@@ -8,16 +8,17 @@ export const skillsData = [
   "Tailwind",
   "MongoDB",
   "MySQL",
-  "PostgreSQL",
   "Git",
   "AWS",
+  "Azure",
   "Bootstrap",
-  "Docker",
-  "Go",
   "Figma",
   "Firebase",
   "MaterialUI",
   "Nginx",
+  "ViteJS",
+  "Wordpress",
+  "Graphql",
 ];
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
