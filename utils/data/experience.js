42c3da1dc3c2cb9f16478ea23f3912291078eb1e
de-salman <salmanfarisi.bca@gmail.com>
2024-07-09 16:39:19 +0530

    Experience content updated

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -9,12 +9,12 @@ export const experiences = [
     id: 2,
     title: "FullStack Developer",
     company: "Admore Digital",
-    duration: "(Jun 2020 - Dec 2021)",
+    duration: "(June 2020 - Dec 2021)",
   },
   {
     id: 3,
     title: "Self Employed",
     company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)",
+    duration: "(April 2019 - June 2020)",
   },
 ];
